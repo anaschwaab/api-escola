9a342cc7dd4f229ea956daf8f4537590878d7786
anaschwaab <goettensana@gmail.com>
2023-04-20 16:36:21 -0300

    pequenas alterações

--- swagger.js
+++ swagger.js
@@ -8,16 +8,16 @@ const options = {
             title: "Soulcode Academy",
             version: "1.0",
             description:
-        "This is a simple CRUD API application made with Express and documented with Swagger",
-            license: {
-                name: "MIT",
-                url: "https://spdx.org/licenses/MIT.html",
-            },
-            contact: {
-                name: "LogRocket",
-                url: "https://logrocket.com",
-                email: "info@email.com",
-            },
+        "Essa é uma CRUD API feita com Express e documentada com Swagger",
+            // license: {
+            //     name: "MIT",
+            //     url: "https://spdx.org/licenses/MIT.html",
+            // },
+            // contact: {
+            //     name: "LogRocket",
+            //     url: "https://logrocket.com",
+            //     email: "info@email.com",
+            // },
         }  ,
     servers: [
         {
